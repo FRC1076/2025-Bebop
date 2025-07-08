84043b92da416cf3fc91794cf8ca17ead01b4c12
pikitty314 <pikitty314@users.noreply.github.com>
2025-07-08 14:04:33 -0400

    Licensing stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,11 @@
-// Copyright 2021-2025 FRC 6328
-// http://github.com/Mechanical-Advantage
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation or
-// available in the root directory of this project.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// Copyright (c) FRC 1076 PiHi Samurai
+// You may use, distribute, and modify this software under the terms of
+// the license found in the root directory of this project
+
+// The contents of this file are based upon those found in the
+// AdvantageKit Skeleton Template, which was created by FRC team 6328,
+// and falls under the GNU General Public License version 3,
+// found in AdvantageKit-License.md in the root directory of the project
 
 package frc.robot;
 
