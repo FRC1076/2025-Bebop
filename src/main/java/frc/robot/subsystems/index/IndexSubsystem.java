c035b645b1d1476c8e31790082bc5bcab631c7dc
pikitty314 <pikitty314@users.noreply.github.com>
2025-08-15 16:38:24 -0400

    Add runVolts() commands
    Co-authored-by: DanielKP123480 <DanielKP123480@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/index/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/index/IndexSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.index;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IndexSubsystem extends SubsystemBase {
@@ -18,6 +20,13 @@ public class IndexSubsystem extends SubsystemBase {
 
     public void stop() {
         io.setVoltage(0);
+    }
+
+    public Command runVolts(double volts) {
+        return Commands.run(
+            () -> setVoltage(volts),
+            this
+        );
     }
     
     @Override
--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.intake;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -18,6 +20,13 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public void stop() {
         io.setVoltage(0);
+    }
+
+    public Command runVolts(double volts) {
+        return Commands.run(
+            () -> setVoltage(volts),
+            this
+        );
     }
 
     @Override 
