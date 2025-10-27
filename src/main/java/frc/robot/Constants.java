4f38ab17e55b6a794f04d812de8db82d137c175e
pikitty314 <pikitty314@users.noreply.github.com>
2025-10-27 17:00:27 -0400

    Revert turn PID constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -167,9 +167,9 @@ public final class Constants {
                     public static final double VelocityConversionFactor = PositionConversionFactor / 60; // Converts from RPM to radians/second
 
                     // PID constants
-                    public static final double kP = 0.5;
+                    public static final double kP = 2;
                     public static final double kI = 0.0;
-                    public static final double kD = 2;
+                    public static final double kD = 0.0001;
 
                     // Feedforward constant
                     public static final double kS = 0.12009; // May be better just to leave this as zero
